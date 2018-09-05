d3bbccc9e6d27edae0f337b23aac8ea10fbff0f7
alex <liangqi.sun@xrewin.com>
2018-09-05 18:47:37 +0800

    20

--- Go/code.go
+++ Go/code.go
@@ -41,6 +41,31 @@ func main (){
 	//	l3 = l3.Next
 	//}
 
+	//20. 有效的括号 isValid
+	//给定一个只包括 '('，')'，'{'，'}'，'['，']' 的字符串，判断字符串是否有效。 
+	//有效字符串需满足： 
+	//左括号必须用相同类型的右括号闭合。
+	//左括号必须以正确的顺序闭合。 
+	//注意空字符串可被认为是有效字符串。
+	//示例 1: 
+	//输入: "()"
+	//输出: true 
+	//示例 2:
+	//输入: "()[]{}"
+	//输出: true
+	//示例 3:
+	//输入: "(]"
+	//输出: false
+	//示例 4:
+	//输入: "([)]"
+	//输出: false
+	//示例 5:
+	//输入: "{[]}"
+	//输出: true 
+	s := "{[]}"
+	fmt.Println(isValid(s))
+	
+	
 	//21. 合同两个有序链表 mergeTwoLists
 	//将两个有序链表合并为一个新的有序链表并返回。新链表是通过拼接给定的两个链表的所有节点组成的。
 	//示例： 
@@ -48,17 +73,18 @@ func main (){
 	//输出：1->1->2->3->4->4
 	//l1 := &ListNode{Val : 1, Next : &ListNode{Val : 2, Next : &ListNode{Val : 4, Next: nil}}}
 	//l2 := &ListNode{Val : 1, Next : &ListNode{Val : 3, Next : &ListNode{Val : 4, Next: &ListNode{Val : 5, Next: nil}}}}
-	var l1 *ListNode
-	var l2 *ListNode
-	l3 := mergeTwoLists(l1, l2)
-	fmt.Println(l3)
-	for l3 != nil{
-		fmt.Print(l3.Val)
-		l3 = l3.Next
-	}
+	//var l1 *ListNode
+	//var l2 *ListNode
+	//l3 := mergeTwoLists(l1, l2)
+	//fmt.Println(l3)
+	//for l3 != nil{
+	//	fmt.Print(l3.Val)
+	//	l3 = l3.Next
+	//}
 
 }
 
+//1.
 func twoSum(nums []int, target int) []int {
 	var result []int
 	maps := make(map[int]int)
@@ -75,7 +101,7 @@ func twoSum(nums []int, target int) []int {
 	
 }
 
-
+//2.
 /**
 * Definition for singly-linked list.
 * type ListNode struct {
@@ -109,7 +135,65 @@ func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
 	return l3
 }
 
+//20.
+func isValid(s string) bool {
+	//str := s[1:]
+	//str2 := s[len(s)-1:]
+	//str3 := s[:1]
+	//fmt.Println(str)
+	//fmt.Println(str2)
+	//fmt.Println(str3)
+	str1 := ""
+	if s[:1] =="}" || s[:1] ==")" || s[:1] =="]"{
+		return true
+	}
+	for{
+		str1 += s[:1]
+		s = s[1:]
+		//if s[1:] == "}" || s[1:] == ")" || s[1:] == "]"{
+		//	
+		//}
+		fmt.Println("str1: ", str1)
+		fmt.Println("s: ", s)
+		fmt.Println("str1[len(str1)-1:]: ", str1[len(str1)-1:])
+		switch s[:1] {
+		case "}":
+			if str1[len(str1)-1:] == "{"{
+				str1 = str1[:len(str1)-1]
+				s = s[1:]
+			}else {
+				return false
+			}
+		
+		case ")":
+			if str1[len(str1)-1:] == "}"{
+				str1 = str1[:len(str1)-1]
+				s = s[1:]
+			}else {
+				return false
+			}
+		case "]":
+			if str1[len(str1)-1:] == "["{
+				str1 = str1[:len(str1)-1]
+				s = s[1:]
+			}else {
+				return false
+			}
+		}
+		
+		if s ==""{
+			return true
+		}
+		
+	}
+	if str1 ==""{
+		return true
+	}
+	
+	return false 
+}
 
+//21.
 /**
  * Definition for singly-linked list.
  * type ListNode struct {
